a4c1138cd352aec893d267f6ab3ea008014ee179
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-11-21 16:18:02 +0300

    ban ip if no snapshots received from client

--- server-antiddos/src/useful/AntiDdos.java
+++ server-antiddos/src/useful/AntiDdos.java
@@ -3,7 +3,10 @@ package useful;
 import arc.struct.Seq;
 import arc.util.Http;
 import arc.util.Log;
+import arc.util.Time;
+import arc.util.Timer;
 import arc.util.serialization.Jval;
+import mindustry.gen.Groups;
 
 import static mindustry.Vars.*;
 
@@ -24,5 +27,14 @@ public class AntiDdos {
             netServer.admins.dosBlacklist.addAll(ips);
             Log.info("Added @ GitHub Actions IPs to blacklist.", ips.size);
         }, error -> Log.err("Failed to fetch GitHub Actions IPs", error));
+
+        Timer.schedule(() -> Groups.player.each(
+            player -> Time.timeSinceMillis(player.con.connectTime) > 10000 && Time.timeSinceMillis(player.con.lastReceivedClientTime) <= 0,
+            player -> {
+                Log.warn("Blacklisting IP '@' as potential DOS attack - no snapshots received.", player.con.address);
+                player.con.close();
+                netServer.admins.blacklistDos(player.con.address);
+            }
+        ), 0f, 3f);
     }
 }
