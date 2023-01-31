96183526c9735b9e527c5ed045f847fee2058e4e
Даркнесс <inflexibled@gmail.com>
2023-01-31 22:14:20 +0300

    Ади не бей

--- server-menus/src/useful/menu/impl/ListMenu.java
+++ server-menus/src/useful/menu/impl/ListMenu.java
@@ -22,8 +22,8 @@ public class ListMenu extends Menu {
         return this;
     }
 
-    public MenuView show(Player player, Func<Integer, String> title, Func<Integer, String> content) {
-        return show(player, menu -> {
+    public MenuView show(Player player, int _page, int _pages, Func<Integer, String> title, Func<Integer, String> content) {
+        return showWith(player, PAGE, _page, PAGES, _pages, menu -> {
             menu.title(title.get(menu.get(PAGE)));
             menu.content(content.get(menu.get(PAGE)));
 
