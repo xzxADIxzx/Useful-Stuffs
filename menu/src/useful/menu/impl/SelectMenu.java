eb14c81c83e43437232c6abc54a92b6a6e01ea07
Darkness <inflexibled@gmail.com>
2023-05-15 21:00:41 +0300

    игрок в шизе

--- menu/src/useful/menu/impl/SelectMenu.java
+++ menu/src/useful/menu/impl/SelectMenu.java
@@ -2,6 +2,7 @@ package useful.menu.impl;
 
 import arc.func.Func;
 import arc.func.Prov;
+import mindustry.gen.Player;
 import useful.Action2;
 import useful.menu.Menu;
 
@@ -11,7 +12,7 @@ public class SelectMenu extends Menu {
         this(1, content, button, action);
     }
 
-    public <T> SelectMenu(Func<MenuView, Iterable<T>> content, Func<T, String> button, Action2<MenuView, T> action) {
+    public <T> SelectMenu(Func<Player, Iterable<T>> content, Func<T, String> button, Action2<MenuView, T> action) {
         this(1, content, button, action);
     }
 
@@ -19,7 +20,7 @@ public class SelectMenu extends Menu {
         this(close, 1, content, button, action);
     }
 
-    public <T> SelectMenu(String close, Func<MenuView, Iterable<T>> content, Func<T, String> button, Action2<MenuView, T> action) {
+    public <T> SelectMenu(String close, Func<Player, Iterable<T>> content, Func<T, String> button, Action2<MenuView, T> action) {
         this(close, 1, content, button, action);
     }
 
@@ -27,7 +28,7 @@ public class SelectMenu extends Menu {
         this("ui.button.close", optionsPerRow, content, button, action);
     }
 
-    public <T> SelectMenu(int optionsPerRow, Func<MenuView, Iterable<T>> content, Func<T, String> button, Action2<MenuView, T> action) {
+    public <T> SelectMenu(int optionsPerRow, Func<Player, Iterable<T>> content, Func<T, String> button, Action2<MenuView, T> action) {
         this("ui.button.close", optionsPerRow, content, button, action);
     }
 
@@ -38,9 +39,9 @@ public class SelectMenu extends Menu {
         });
     }
 
-    public <T> SelectMenu(String close, int optionsPerRow, Func<MenuView, Iterable<T>> content, Func<T, String> button, Action2<MenuView, T> action) {
+    public <T> SelectMenu(String close, int optionsPerRow, Func<Player, Iterable<T>> content, Func<T, String> button, Action2<MenuView, T> action) {
         this.transform(menu -> {
-            menu.options(optionsPerRow, content.get(menu), button, action).row();
+            menu.options(optionsPerRow, content.get(menu.player), button, action).row();
             menu.option(close);
         });
     }
