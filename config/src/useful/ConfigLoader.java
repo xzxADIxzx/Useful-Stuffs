2673d10f0fbe9b4c7d9cec1aa18ffcb7b46c1f65
Darkness <inflexibled@gmail.com>
2023-08-24 17:26:54 +0300

    fixes

--- config/src/useful/ConfigLoader.java
+++ config/src/useful/ConfigLoader.java
@@ -2,15 +2,9 @@ package useful;
 
 import arc.files.Fi;
 import arc.util.serialization.Json;
-import arc.util.serialization.JsonWriter;
 import arc.util.serialization.JsonWriter.OutputType;
 import arc.util.serialization.Jval;
 import arc.util.serialization.Jval.Jformat;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static mindustry.Vars.dataDirectory;
 
